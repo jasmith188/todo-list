d4c6c8cc5d0cecff6d5155ba86e4e02bac18f8e0
James Smith <jasmith.188@gmail.com>
2021-01-29 16:30:31 -0500

    finish CRUD functionality

--- src/App.js
+++ src/App.js
@@ -4,10 +4,35 @@ import Todo from './Todo';
 import TodoForm from './TodoForm';
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn about React',
+      isCompleted: false,
+    },
+    {
+      text: 'Meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'Build really cool todo app',
+      isCompleted: false,
+    },
+  ]);
 
   const addTodo = (text) => {
     const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
     setTodos(newTodos);
   };
 
@@ -19,7 +44,13 @@ function App() {
       <TodoForm addTodo={addTodo} />
       <div className="todo__list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
         ))}
       </div>
     </div>
--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Todo = ({todo}) => {
+const Todo = ({todo, index, completeTodo, removeTodo}) => {
     return (
-        <div>
+        <div className="todo"
+        style={{ textDecoration: todo.isCompleted ? "line-through" : "" }} >
             {todo.text}
+            
+            <button onClick={() => completeTodo(index)}>Complete</button>
+            <button onClick={() => removeTodo(index)}>x</button>
+     
         </div>
     )
 }
